11c7f13e9b4178433791e84f62e8e4e415b0247d
mirae-Jo <mirae9873@gmail.com>
2024-04-30 22:32:49 +0900

    메인 페이지 clinet component -> server component 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-import { useEffect, useState } from "react";
 import Align from "./_components/main/Align";
 import BgCoverImage from "./_components/main/BgCoverImage";
 import mainWaveGif from "@/../../public/wave.gif";
@@ -13,55 +11,39 @@ const purple = Purple_Purse({
   weight: ["400"],
 });
 
-const loadPage = () => {
-  return (
-    <div className='bg-white/70 absolute top-0 left-0 w-full h-full animate-fade-in-slow'>
-      <div className='absolute bottom-[16rem] left-0 right-[6.5rem] flex flex-col gap-[5.7rem]'>
-        <Align
-          className={`${purple.className} text-[9.6rem] text-main-blue w-[82.2rem] h-[22.5rem] leading-[9.6rem] self-end`}>
-          <Align.Left>
-            <span>FRONT-END</span>
-          </Align.Left>
-          <Align.Right>
-            <span>DEVELOPER</span>
-            <Link href={"/info"} className='mb-[0.5rem]'>
-              <Image
-                className='animate-bounce-horizontal'
-                src={rightArrow}
-                alt={"인포 페이지로 이동하기"}
-              />
-            </Link>
-          </Align.Right>
-        </Align>
-        <Align className='gap-[1.6rem] w-full pr-[9rem] text-[3.6rem] text-main-blue'>
-          <Align.Right>
-            <span>2024</span>
-          </Align.Right>
-          <hr className='border-main-blue' />
-          <Align.Right>
-            <span>MIRAE's Portfolio</span>
-          </Align.Right>
-        </Align>
-      </div>
-    </div>
-  );
-};
-
 export default function Home() {
-  const [showLoadPage, setShowLoadPage] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowLoadPage(true);
-    }, 1000);
-
-    return () => clearTimeout(timer); // Cleanup function
-  }, []);
-
   return (
-    <div className='w-screen h-auto relative'>
+    <div className='w-screen h-screen relative'>
       <BgCoverImage src={mainWaveGif} alt={"main wave gif"} />
-      {showLoadPage && loadPage()}
+      <div className='bg-white/70 absolute top-0 left-0 w-full h-full animate-fade-in-slow opacity-0'>
+        <div className='absolute bottom-[16rem] left-0 right-[6.5rem] flex flex-col gap-[5.7rem]'>
+          <Align
+            className={`${purple.className} text-[9.6rem] text-main-blue w-[82.2rem] h-[22.5rem] leading-[9.6rem] self-end`}>
+            <Align.Left>
+              <span>FRONT-END</span>
+            </Align.Left>
+            <Align.Right>
+              <span>DEVELOPER</span>
+              <Link href={"/info"} className='mb-[0.5rem]'>
+                <Image
+                  className='animate-bounce-horizontal'
+                  src={rightArrow}
+                  alt={"인포 페이지로 이동하기"}
+                />
+              </Link>
+            </Align.Right>
+          </Align>
+          <Align className='gap-[1.6rem] w-full pr-[9rem] text-[3.6rem] text-main-blue'>
+            <Align.Right>
+              <span>2024</span>
+            </Align.Right>
+            <hr className='border-main-blue' />
+            <Align.Right>
+              <span>MIRAE's Portfolio</span>
+            </Align.Right>
+          </Align>
+        </div>
+      </div>
     </div>
   );
 }
